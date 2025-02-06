13145fe92913a08a245860d1edb1c8b06f2d3d4a
MoMoizUrRehman <bscs24083@itu.edu.pk>
2025-02-06 16:28:12 +0500

    Audio added

--- MetalSlug.cpp
+++ MetalSlug.cpp
@@ -70,14 +70,18 @@ void explosion_Collision(Rectangle* collider1, Rectangle* collider2) {
 
 int main() {
     InitWindow(screenWidth, screenHeight, "Metal Slug Prototype");
+    InitAudioDevice();
     SetTargetFPS(60);
     Bullet bullet_mp;
     Texture2D background1 = LoadTexture("bg1.png");
     Texture2D background2 = LoadTexture("bg2.png");
     Texture2D MainPlayerTexture = LoadTexture("player.png");
     Texture2D Startbg = LoadTexture("Start_Screen.png");
-    bullet_mp.bullet_texture = LoadTexture("bullet.png");
-   
+    bullet_mp.bullet_texture = LoadTexture("bullet.png"); // loaded
+    Music start_music;
+    start_music = LoadMusicStream("start_audio.mp3");
+    Sound jump = LoadSound("jump.mp3");
+    Music ingame = LoadMusicStream("Ingame.mp3");
     float bg1 = 0;
     float bg2 = screenWidth;
     float sbg = 0;
@@ -102,14 +106,14 @@ int main() {
     const float gravity = 0.7f;
     const float jumpForce = -12.5f;
     bool isJumping = false;
-    
-
+    PlayMusicStream(start_music);
     bool startScreen = true;
-
     while (!WindowShouldClose()) {
         if (startScreen) {
             BeginDrawing();
+            
             ClearBackground(RAYWHITE);
+            UpdateMusicStream(start_music);
             DrawTexturePro(
                 Startbg,
                 { 0, 0, (float)Startbg.width, (float)Startbg.height },  // Source rectangle
@@ -118,15 +122,23 @@ int main() {
                 0.0f,      // Rotation
                 WHITE
             );
-     
             EndDrawing();
+            bullet_mp.x = player.x + 75;
+            bullet_mp.y = player.y + 70;
 
             if (IsKeyPressed(KEY_ENTER)) {
                 startScreen = false;
+                UnloadMusicStream(start_music);
+                PlayMusicStream(ingame);
             }
+            
         }
         else 
         {
+            if (!startScreen) {
+                UpdateMusicStream(ingame);
+            }
+
             bg1 -= bgSpeed * GetFrameTime();
             bg2 -= bgSpeed * GetFrameTime();
 
@@ -143,6 +155,7 @@ int main() {
             }
 
             if ((IsKeyPressed(KEY_SPACE) && !isJumping) || (IsKeyPressed(KEY_W) && !isJumping)) {
+                PlaySound(jump);
                 player.velocityY = jumpForce;
                 isJumping = true;
             }
@@ -160,15 +173,18 @@ int main() {
 
             if (player.x < 0) player.x = 0;
             if (player.x > screenWidth - 150) player.x = screenWidth - 150;
-
+            bullet_mp.bullet_texture.height = 100;
+            bullet_mp.bullet_texture.width = 100;
             BeginDrawing();
             ClearBackground(RAYWHITE);
             DrawTextureEx(background1, { bg1, 0 }, 0.0f, (float)screenWidth / background1.width, WHITE);
             DrawTextureEx(background2, { bg2, 0 }, 0.0f, (float)screenWidth / background2.width, WHITE);
             DrawTextureEx(MainPlayerTexture, { (float)player.x, (float)player.y }, 0.0f, scaleFactor, WHITE);
+            
             EndDrawing();
         }
     }
+    UnloadMusicStream(ingame);
     UnloadTexture(Startbg);
     UnloadTexture(background1);
     UnloadTexture(MainPlayerTexture);
